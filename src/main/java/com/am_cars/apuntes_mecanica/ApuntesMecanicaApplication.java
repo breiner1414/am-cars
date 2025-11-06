ad5f7c1a0e95c1a807bd3f5d364ba37afa27ff97
breiner14 <101745974+breiner14@users.noreply.github.com>
2025-11-06 11:21:07 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/am_cars/apuntes_mecanica/ApuntesMecanicaApplication.java
@@ -0,0 +1,13 @@
+package com.am_cars.apuntes_mecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuntesMecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuntesMecanicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/am_cars/apuntes_mecanica/ApuntesMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am_cars.apuntes_mecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuntesMecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
